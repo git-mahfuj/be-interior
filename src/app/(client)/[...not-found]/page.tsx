558e37b941826f23c29539649fba609a7d4ce694
the_NOOB <mahfujurrheeck@gmail.com>
2026-06-10 23:00:01 +0600

    Update page.tsx

--- src/app/(client)/[...not-found]/page.tsx
+++ src/app/(client)/[...not-found]/page.tsx
@@ -3,5 +3,5 @@
 import { notFound } from "next/navigation";
 
 export default function CatchAllNotFound() {
-  notFound(); // এটি আপনার (client) এর ভেতরের not-found.tsx ফাইলকে জোর করে ট্রিগার করাবে
+  notFound();
 }
